5d7af33184ee528530718ac46e8b6bc638c89f04
Meheruba Hasin <87016852+meherubahasin@users.noreply.github.com>
2025-01-04 13:04:39 +0600

    Update app.py
    Added the 'veiw cart' feature for customers to see their purchase

--- app.py
+++ app.py
@@ -19,7 +19,22 @@ product_collections = db.products
 def display_products():
     products = product_collections.find()
 
-    return render_template('shop.html', products=products)
+    cart_items = list(cart_collections.find())
+    products = list(product_collections.find())
+    cart_details = []
+    n = 0
+    for item in cart_items:
+        product = product_collections.find_one({"_id": ObjectId(item['product_id'])})
+        if product:
+            cart_details.append({
+                "id":  ObjectId(item['_id']),
+                "name": product["name"],
+                "quantity": item["quantity"],
+                "price": product["price"],
+                "total": product["price"] * int(item["quantity"])
+            })
+    print(cart_items)
+    return render_template('shop.html', products=products, cart_items=cart_details, n=n)
 
   
 @app.route('/add_to_cart', methods = ['POST', 'GET'])
@@ -32,6 +47,19 @@ def add_to_cart():
             # Insert the order into the cart collection
             cart_collections.insert_one({'product_id': product_id, 'quantity': int(quantity)})
             return redirect(url_for('display_products'))
-  
+        
+@app.route('/update_cart', methods = ['POST', 'GET'])
+def update_cart():
+        if request.method == 'POST':
+            product_id = request.form.get('product_id')
+            product_id = ObjectId(product_id)
+            quantity = request.form.get('quantity')
+        if product_id and quantity:
+            # Insert the order into the cart collection
+            cart_collections.insert_one({'product_id': product_id, 'quantity': int(quantity)})
+            return redirect(url_for('display_products'))
+        
+
+
 if __name__ == '__main__': 
     app.run(debug=True) 
